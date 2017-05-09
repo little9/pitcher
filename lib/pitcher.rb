33aec8d967c9dc7edf113b085e46a3f1bb60190e
adehner <dehner.a@gmail.com>
2017-05-09 15:03:45 -0700

    printing response

--- lib/pitcher.rb
+++ lib/pitcher.rb
@@ -54,6 +54,7 @@ module Pitcher
       )
       # Create the hash that will be sent as a message to the service with the creds and data above
       response = client.call(:process_conten_tdm, message: message)
+      print response.to_hash
     end
   end
 end
